b3979d53afadc5220abfb62ce1d727e7284be6bb
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-02-18 21:51:25 +0100

    make router imports local

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ from fastapi.templating import Jinja2Templates
 
 from .config import ConfigType, get_db_url, settings
 from .data.db import init_db
-from .routers.library import router as library_router
-from .routers.open import router as open_router
 
 BASE_PATH = Path(__file__).resolve().parent
 template_folder = str(BASE_PATH / "templates")
@@ -16,30 +14,35 @@ templates = Jinja2Templates(directory=template_folder)
 
 
 def create_app(config_type: ConfigType = ConfigType.DEVELOPMENT) -> FastAPI:
-    _app = FastAPI(title=settings.PROJECT_TITLE,
-                   version=settings.PROJECT_VERSION)
+    app = FastAPI(title=settings.PROJECT_TITLE,
+                  version=settings.PROJECT_VERSION)
 
-    configure(_app)
-    register_events(_app, config_type)
-    register_routers(_app)
+    configure(app)
+    register_events(app, config_type)
+    register_routers(app)
 
-    return _app
+    return app
 
 
-def configure(_app: FastAPI) -> None:
+def configure(app: FastAPI) -> None:
     fastapi_jinja.global_init(template_folder, auto_reload=True)
-    _app.mount("/static", StaticFiles(directory=str(BASE_PATH / "static")), name="static")
+    app.mount("/static", StaticFiles(directory=str(BASE_PATH / "static")), name="static")
 
 
-def register_events(_app: FastAPI, config_type: ConfigType) -> None:
-    @_app.on_event("startup")
+def register_events(app: FastAPI, config_type: ConfigType) -> None:
+    @app.on_event("startup")
     async def on_startup():
-        await init_db(_app, get_db_url(config_type))
+        await init_db(app, get_db_url(config_type))
 
         if settings.DEBUG:
             print(settings)
 
 
-def register_routers(_app: FastAPI) -> None:
-    _app.include_router(library_router, tags=["library"])
-    _app.include_router(open_router, tags=["open"])
+def register_routers(app: FastAPI) -> None:
+    from .routers.ajax import router as ajax_router
+    from .routers.library import router as library_router
+    from .routers.open import router as open_router
+
+    app.include_router(ajax_router, prefix="/ajax", tags=["ajax"])
+    app.include_router(library_router, tags=["library"])
+    app.include_router(open_router, tags=["open"])
